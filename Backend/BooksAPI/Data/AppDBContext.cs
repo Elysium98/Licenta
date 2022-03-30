45a821d0b5c7d6ceed6ced493b0ad07d24e151d5
Sirbu Alexandru-Paul <justst4r@gmail.com>
2022-03-30 22:24:07 +0300

    added folder structures and connection to SQL SERVER

---
+++ Backend/BooksAPI/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using BooksAPI.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksAPI.Data
+{
+    public class AppDBContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Backend/BooksAPI/Data/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BooksAPI.Data.Entities
+{
+    public class User : IdentityUser
+    {
+
+        public string FullName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
